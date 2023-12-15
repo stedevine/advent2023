71c3768239ef898cb1d790499050f6878fb59e14
Ste Devine <ste@stedevine.com>
2023-12-15 14:49:06 +0000

    refactor

--- 10/10.py
+++ 10/10.py
@@ -6,73 +6,56 @@ def get_start_position(lines):
 
 def get_next_step(r,c,lines):
     if r > 0 and lines[r-1][c] in ('|F7'):
-        return (r-1,c, 'up')
+        return (r-1,c, 'U')
     if r < len(lines) and lines[r+1] in ('|JL'):
-        return (r+1,c, 'down')
+        return (r+1,c, 'D')
     if c > 0 and lines[r][c-1] in ('-FL'):
-        return (r, c-1, 'left')
+        return (r, c-1, 'L')
     if c < len(lines[r]) and lines[r][c+1] in ('-J7'):
-        return (r,c+1, 'right')
-    
-    print('error')
-    
+        return (r,c+1, 'R')
+
+# How do you make the arrows ↑ ↓ → ← on your keyboard? 
 def part_1(lines):
     # find S
     # find one of the two pipes that connect S to the loop
     # follow the loop until we get back to S
     # result is number of steps taken // 2
     r,c = get_start_position(lines)
-    print(f'{r}{c}')
+    print(f'start: ({r},{c})')
     steps = 1
     r,c, direction = get_next_step(r,c,lines)
-    #print(lines[r][c])
+    
+    #print(f'first : {direction} : {lines[r][c]}')
+    dp = direction + lines[r][c]
     while True:
-        position = lines[r][c]
-        if position == 'S':
+        #print(f'dp: {dp} ({r},{c})') 
+        #position = lines[r][c]
+        if dp[-1] == 'S':
             break
-        #print(f'{steps} : {direction} {r},{c} {lines[r][c]}' )
-        if direction == 'up':
-            if position == '|':
-                r -= 1
-                direction = 'up'
-            elif position == 'F':
-                c += 1
-                direction = 'right'
-            else: # left
-                c -= 1
-                direction = 'left'
+        
+        #dp = direction + position
+        if   (dp == 'U|') : d = 'U'
+        elif (dp == 'UF') : d = 'R'
+        elif (dp == 'U7') : d = 'L'
 
-        elif direction == 'down':
-            if position == '|':
-                r += 1
-                direction = 'down'
-            elif position == 'J':
-                c -= 1
-                direction = 'left'
-            else: # right
-                c += 1
-                direction = 'right'
+        elif (dp == 'D|') : d = 'D'
+        elif (dp == 'DJ') : d = 'L'
+        elif (dp == 'DL') : d = 'R'
 
-        elif direction == 'right':
-            if position == '7':
-                r += 1
-                direction = 'down'
-            elif position == 'J':
-                r -= 1
-                direction  = 'up'
-            else: # right
-                c += 1
-                direction = 'right'
-        else: # 'left'
-            if position == 'F':
-                r += 1
-                direction = 'down'
-            elif position == 'L':
-                r -= 1
-                direction = 'up'
-            else: # left
-                c -= 1 
-                direction = 'left' 
+        elif (dp == 'L-') : d = 'L'
+        elif (dp == 'LF') : d = 'D'
+        elif (dp == 'LL') : d = 'U'
+
+        elif (dp == 'R-') : d = 'R'
+        elif (dp == 'R7') : d = 'D'
+        else:               d = 'U'
+        
+        if      d == 'U':   r -=1
+        elif    d == 'D':   r +=1
+        elif    d == 'R':   c +=1
+        else:               c -=1
+
+        dp = d + lines[r][c]
         
         steps += 1
 
@@ -91,7 +74,7 @@ SJ.L7
 |F--J
 LJ..."""
 
-for t in [test1,test2]:
+for t in [test2]:
     r = part_1(t.split('\n'))
     print(f"test result {r}")
 
